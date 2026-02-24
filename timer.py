8bb52c75fad6cb6b9e631651ca048f2be83da746
Jorge Cano <coronero@gmail.com>
2026-02-24 10:21:08 -0800

    implemented basic timer, need to add sound next

--- timer.py
+++ timer.py
@@ -1,5 +1,29 @@
 import time
+import os
 
-cTime = time.ctime()
-print(f"The time is: {cTime}")
+def clear():
+    os.system("clear")
+
+def studyTimer(minutes):
+    total_seconds = minutes * 60 #converting minutes to seconds to count down until 0
+
+    while(total_seconds):
+        min, sec = divmod(total_seconds, 60)
+        timer = f"{min:02d}:{sec:02d}"
+
+        clear()
+        print("Study time")
+        print("--------------")
+        print(timer)
+
+        time.sleep(1)
+        total_seconds -= 1
+
+    clear()
+    print("Time is up.") 
+
+minutes = int(input("Enter time you want to study: "))
+studyTimer(minutes)
+        
+
 
